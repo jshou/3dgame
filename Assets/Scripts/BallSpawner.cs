88b0c569c16de2076b0b90ea4fc49ab85d496af0
jshou <joshuahou@gmail.com>
2014-01-08 11:31:00 -0800

    spawn balls when clicking

---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallSpawner : MonoBehaviour {
+  public GameObject BallPrefab;
+
+  void Update() {
+    if (Input.GetMouseButtonDown(0)) {
+      var ball = GameObject.Instantiate(BallPrefab, transform.position, transform.rotation) as GameObject;
+      ball.GetComponent<Rigidbody>().AddForce(transform.forward * 1500, ForceMode.Impulse);
+    }
+  }
+}
