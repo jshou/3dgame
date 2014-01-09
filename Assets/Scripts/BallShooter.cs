1a9be86b877555fb70cb72580567609411e4fdab
jshou <joshuahou@gmail.com>
2014-01-09 14:57:55 -0800

    activate ball shooter

---
+++ Assets/Scripts/BallShooter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallShooter : MonoBehaviour {
+  void Update() {
+    if (Input.GetMouseButtonDown(1)) {
+      Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+    }
+  }
+}
