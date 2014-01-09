bcde5fdd1b7f453df93d200c8cbe0d52eb7862b2
jshou <joshuahou@gmail.com>
2014-01-09 14:43:50 -0800

    tomato crosshairs

---
+++ Assets/Scripts/CrosshairDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrosshairDrawer : MonoBehaviour {
+  public Texture2D CrosshairTexture;
+  private Rect Position;
+  private const int CROSSHAIR_SIZE = 100;
+
+  void Start() {
+    var crossX = (Screen.width - CROSSHAIR_SIZE) / 2;
+    var crossY = (Screen.height - CROSSHAIR_SIZE) / 2;
+
+    Position = new Rect(crossX, crossY, CROSSHAIR_SIZE, CROSSHAIR_SIZE);
+  }
+
+  void OnGUI() {
+    GUI.DrawTexture(Position, CrosshairTexture);
+  }
+}
