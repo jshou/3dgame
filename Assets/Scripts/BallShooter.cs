6347d8d10770e7ccc4cf69d5eaffda749b42a32c
jshou <joshuahou@gmail.com>
2014-01-09 15:16:41 -0800

    ball shrinker

--- Assets/Scripts/BallShooter.cs
+++ Assets/Scripts/BallShooter.cs
@@ -4,7 +4,16 @@ using System.Collections;
 public class BallShooter : MonoBehaviour {
   void Update() {
     if (Input.GetMouseButtonDown(1)) {
-      Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+      var ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+
+      RaycastHit hit;
+      if (Physics.Raycast(ray, out hit)) {
+        var hittee = hit.transform.gameObject;
+        var ballShrinker = hittee.GetComponent<BallShrinker>();
+        if (ballShrinker != null) {
+          ballShrinker.Shrink();
+        }
+      }
     }
   }
 }
---
+++ Assets/Scripts/BallShrinker.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallShrinker : MonoBehaviour {
+  public void Shrink() {
+    gameObject.transform.localScale = new Vector3(2, 2, 2);
+  }
+}
